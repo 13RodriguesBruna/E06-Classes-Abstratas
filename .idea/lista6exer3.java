12a6aa187cdb97c93d686055223d8791c806ed49
13RodriguesBruna <107412241+13RodriguesBruna@users.noreply.github.com>
2022-10-06 18:09:58 -0300

    Add files via upload

---
+++ .idea/lista6exer3.java
@@ -0,0 +1,31 @@
+//criar uma super  classe  Conta
+// criar subclasses que herder 
+//Todas essas contas deverão herdar suas características de Conta
+
+public class Main {
+        public static void main(String[]args){
+        cliente pfisi = new ClientePF();
+        cliente pjuri = new ClientePJ();
+        
+        if(pfisi.autenticar(()){
+        System.out.println("A conta está autenticada");
+        
+        }else {
+        System.out.println("Conta não está autenticada);
+        a
+        }
+
+
+        }
+
+//METODO AUTENTICAR, vou usar chave dono e cpf
+            //ClientePessoaFisica: A autenticação deverá comparar se a chave é igual ao CPF.
+             public boolean autenticar1(String chave){
+
+                 if(chave.equals(this.numero)){
+                     return true;a
+            }
+                 else
+                     return false;
+             }
+        }
