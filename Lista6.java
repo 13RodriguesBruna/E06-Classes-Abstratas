cceadbcc2e8d278e4bbb3e6d85e3da046ab5c7c2
13RodriguesBruna <107412241+13RodriguesBruna@users.noreply.github.com>
2022-10-06 17:33:59 -0300

    Lista6exer1

---
+++ Lista6.java
@@ -0,0 +1,41 @@
+//classe conta
+public class Conta abstract{
+
+    //  public static void main(String[] args) {
+//atributos
+    int numero;
+    String dono;
+    double saldo;
+    double limite;
+
+    //classe main
+    public class Main {
+        public static void main(String[]args){
+
+            Conta contaminha = new Conta();
+            Conta contaIron = new Conta();a
+
+            // manipulação contaminha
+
+            contaminha.dono= "Bruna";
+            contaminha.saldo=100000.00;
+            contaminha.numero=13;
+            contaminha.limite=1000.00;
+
+            contaIron.dono= "Iron";
+            contaIron.saldo = 10000.0;
+            contaIron.numero=3;
+            contaIron.limite=15000.00
+
+            System.out.println("Conta Bruna informações:");
+
+            System.out.println("Nome conta:"+contaminha.dono);
+            System.out.println("Saldo conta:"+contaminha.saldo);
+            System.out.println("Número conta:"+contaminha.numero);
+            System.out.println("Limite conta"+contaminha.limite);
+
+            System.out.println("\nConta Iron informações:");
+            System.out.println("Nome conta:"+contaIron.dono);
+            System.out.println("Saldo conta:"+contaIron.saldo);
+            System.out.println("Limite conta"+contaIron.limite);
+
